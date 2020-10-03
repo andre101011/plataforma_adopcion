49526604695a45248257388d14749b3579ed1bbe
AndresLlinas <andreslli1707@gmail.com>
2020-10-02 21:47:04 -0500

    Bugs fixed, login added
    Responsive bugs in index were fixed
    Social network icon bar added
    Login styles added

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -3,6 +3,11 @@ const router= express.Router();
 
 const pool= require('../database');
 
+
+router.get('/login',(req,res)=>{
+    res.render('employees/login');
+})
+
 router.get('/add',(req,res)=>{
     res.render('employees/add');
 })

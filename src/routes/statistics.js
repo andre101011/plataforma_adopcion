e43cc1eb71597c5607ab989d1e3ed0ce19edd481
Neyder Figueroa <nfigueroas@uqvirtual.edu.co>
2020-10-23 12:52:41 -0500

    Interfaz base para la primera entrega

--- src/routes/statistics.js
+++ src/routes/statistics.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+
 const pool = require("../database");
 
-router.get('/', async (req,res)=>{
+router.get("/", async (req, res) => {
+  const empleados = await pool.query("SELECT * FROM Empleado");
+  var data = [15, 10, 30];
 
-    //const empleados = await pool.query('SELECT * FROM Empleado'); 
-    const data=[15,10,30]
 
-    
-    
-    res.render('statistics/test',{data:data});
-
-})
-
+  res.render("statistics/test", { data: data });
+});
 
 module.exports = router;

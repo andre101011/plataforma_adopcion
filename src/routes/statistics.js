397040827908803090ab096bbb13960203ee0043
AndresLlinas <andreslli1707@gmail.com>
2020-11-16 14:51:37 -0500

    Cambios  en estadisticas

--- src/routes/statistics.js
+++ src/routes/statistics.js
@@ -94,22 +94,25 @@ router.post("/stat2", async (req, res) => {
   });
 });
 
-/**  numero de machos, hembras y total en la bd
-  const result = await pool.query(
-    "SELECT COUNT(CASE WHEN sexo =\"macho\" THEN id_animal END) AS machos, COUNT(CASE WHEN sexo =\"hembra\" THEN id_animal END) AS hembras, COUNT(*) AS Total FROM Animal;"
-  );
-*/
-/** 
-   * datos de animales entregados - año especifico
-  const result = await pool.query(
-    "SELECT especie,sexo,sitio_rescate,fecha_entrega FROM animal a INNER JOIN (SELECT *FROM  adopcion where year(fecha_entrega)=2020 GROUP BY id_animal) b ON a.id_animal = b.id_animal;"
+router.post("/stat3", async (req, res) => {
+  var { barraano } = req.body;
+  var barraano = barraano;
+  console.log(barraano);
+
+  var result = await pool.query(
+    'SELECT COUNT(CASE WHEN sexo ="macho" THEN id_animal END) AS machos, COUNT(CASE WHEN sexo ="hembra" THEN id_animal END) AS hembras, COUNT(*) AS Total FROM Animal WHERE estado<>"adoptado";'
   );
 
-  /** agrupar # de adopciones por mes - todos los años
-const result = await pool.query(
-  "select date_format(fecha_entrega,'%Y-%b') as fecha, count(id_animal) as entregados from adopcion group by year(fecha_entrega),month(fecha_entrega) order by year(fecha_entrega),month(fecha_entrega);"
-);
-*/
+  console.log(result);
+  valuesH = extractAttributeValuesFromJsonArray(result, "hembras");
+  valuesM = extractAttributeValuesFromJsonArray(result, "machos");
+
+  res.render("statistics/statistics", {
+    valuesH: valuesH[0],
+    valuesM: valuesM[0],
+    stat: "stat3",
+  });
+});
 
 module.exports = router;
 

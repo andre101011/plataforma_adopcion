b951082ca4376f9e6d9568ca1665ecf4457db333
Neyder Figueroa <nfigueroas@uqvirtual.edu.co>
2020-11-18 10:46:59 -0500

    cambios en los identificadores de los botones

--- cypress/integration/pruebas/sample-spec.js
+++ cypress/integration/pruebas/sample-spec.js
@@ -4,27 +4,3 @@ describe("Pagina principal", () => {
   });
 });
 
-describe("La pagina de login", () => {
-  context("Startup", () => {
-    beforeEach(() => {
-      cy.visit("/employees/login");
-    });
-
-    it("loggearse", () => {
-      // Fill the username
-      cy.get('input[name="email"]')
-        .type("nfigueroasan@gmail.com")
-        .should("have.value", "nfigueroasan@gmail.com");
-
-      // Fill the password
-      cy.get('input[name="password"]')
-        .type("1234")
-        .should("have.value", "1234");
-
-      // Locate and submit the form
-      cy.get("login-form").submit();
-
-      cy.url().should("include", "/animals");
-    });
-  });
-});

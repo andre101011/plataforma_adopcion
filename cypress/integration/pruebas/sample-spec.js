aed0b1f371e85fcbddb89d6287e2ec8b2387e4ca
Neyder Figueroa <nfigueroas@uqvirtual.edu.co>
2020-11-18 10:12:07 -0500

    prueba de inicio de sesio

--- cypress/integration/pruebas/sample-spec.js
+++ cypress/integration/pruebas/sample-spec.js
@@ -1,6 +1,38 @@
-describe("login usuario",
+describe("Pagina principal",
   () => {
-    it("Ir a la pagina de login", () => {
-      cy.visit("https://adopciones-fundamor.herokuapp.com/")
+    it("ir a la pagina principal", () => {
+      cy.visit("/")
+    })
+  })
+
+
+  describe('The Login Page', () => {
+    beforeEach(() => {
+      // reset and seed the database prior to every test
+      cy.exec('npm run db:reset && npm run db:seed')
+  
+      // seed a user in the DB that we can control from our tests
+      // assuming it generates a random password for us
+      cy.request('POST', '/employees/login', { username: 'nfigueroas@gmail.com', password:'1234'})
+        .its('body')
+        .as('currentUser')
+    })
+  
+    it('sets auth cookie when logging in via form submission', function () {
+      // destructuring assignment of the this.currentUser object
+      const { username, password } = this.currentUser
+  
+      cy.visit('/employees/login')
+  
+      cy.get('input[name=email]').type(username)
+  
+      // {enter} causes the form to submit
+      cy.get('input[name=password]').type(`${password}{enter}`)
+  
+      // we should be redirected to /dashboard
+      cy.url().should('include', '/animals')
+  
+  
+
     })
   })

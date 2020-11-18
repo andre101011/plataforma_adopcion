45bdbcd18d47701c4e0643dc2dfbedd233ef8089
Neyder Figueroa <nfigueroas@uqvirtual.edu.co>
2020-11-18 10:50:16 -0500

    prueba de inicio de sesión 6

--- cypress/integration/pruebas/sample-spec.js
+++ cypress/integration/pruebas/sample-spec.js
@@ -4,3 +4,27 @@ describe("Pagina principal", () => {
   });
 });
 
+describe("La pagina de login", () => {
+  context("Startup", () => {
+    beforeEach(() => {
+      cy.visit("/employees/login");
+    });
+
+    it("loggearse", () => {
+      // Fill the username
+      cy.get('input[name="email"]')
+        .type("nfigueroasan@gmail.com")
+        .should("have.value", "nfigueroasan@gmail.com");
+
+      // Fill the password
+      cy.get('input[name="password"]')
+        .type("1234")
+        .should("have.value", "1234");
+
+      // Locate and submit the form
+      cy.get('button[name="ingresar"]').click();
+
+      cy.url().should("include", "/animals");
+    });
+  });
+});

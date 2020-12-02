81d857114c463ef53da28b233301295a23c489f9
Neyder Figueroa <nfigueroas@uqvirtual.edu.co>
2020-12-02 18:24:40 -0500

    stat2 mode

--- src/routes/statistics.js
+++ src/routes/statistics.js
@@ -23,7 +23,7 @@ router.get('/',(req,res)=>{
 router.get("/stat1", async (req, res) => {
  
   
-  const mode = await pool.query(
+  await pool.query(
     "SET sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'"
   );
   const result = await pool.query(
@@ -44,7 +44,9 @@ router.get("/stat1", async (req, res) => {
 
 router.post("/stat1", async (req, res) => {
   
-
+   await pool.query(
+    "SET sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'"
+  );
   const result = await pool.query(
     "select date_format(fecha_entrega,'%Y-%b') as fecha, count(id_animal) as entregados from Adopcion where year(fecha_entrega)=2020 group by year(fecha_entrega),month(fecha_entrega)  order by year(fecha_entrega),month(fecha_entrega);"
   );
@@ -55,10 +57,13 @@ router.post("/stat1", async (req, res) => {
   res.render("statistics/stat1", { labels: labels, values: values });
 });
 router.get("/stat2", async (req, res) => {
+  await pool.query(
+    "SET sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'"
+  );
   var result = await pool.query(
     "SELECT  date_format(fecha_entrega,'%Y-%b') as mes  , SUM(CASE WHEN sexo = 'macho' THEN 1 ELSE 0 END) AS machos  , SUM(CASE WHEN sexo = 'hembra' THEN 1 ELSE 0 END) AS hembras FROM  Animal INNER JOIN Adopcion ON Animal.id_animal = Adopcion.id_animal group by year(fecha_entrega),month(fecha_entrega)  order by year(fecha_entrega),month(fecha_entrega);"
   );
-  console.log(result);
+  
   labels = extractAttributeValuesFromJsonArray(result, "mes");
   valuesH = extractAttributeValuesFromJsonArray(result, "hembras");
   valuesM = extractAttributeValuesFromJsonArray(result, "machos");
@@ -79,6 +84,9 @@ router.post("/stat2", async (req, res) => {
   var { barraano } = req.body;
   var barraano = barraano;
 
+  const mode = await pool.query(
+    "SET sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'"
+  );
 
   var result = await pool.query(
     `SELECT  date_format(fecha_entrega,'%Y-%b') as mes ,
